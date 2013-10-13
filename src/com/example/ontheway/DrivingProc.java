f361a07b9c7b9a7b98ec3783cbc83669d3d74bcb
BrianB86 <bbattell86@gmail.com>
2013-10-12 23:15:46 -0400

    added skeleton to process

---
+++ src/com/example/ontheway/DrivingProc.java
@@ -0,0 +1,23 @@
+package com.example.ontheway;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class DrivingProc extends IntentService  {
+	
+	public DrivingProc(String name) {
+		super(name);
+		//grab users contacts
+		//grab mapped responses
+		
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onHandleIntent(Intent workIntent) {
+		//majority of code should go here. Running process that checks for driving.
+		//dies only when checked off by main activity
+		
+	}
+
+}
